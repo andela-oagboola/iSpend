eee8b345abc4c49eff219ff326bac7e4d15a8747
Andela Dev <olaide.agboola@andela.co>
2015-03-26 15:46:46 +0100

    fixed db connection

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   developmentUrl: "mongodb://localhost/expense-manager",
-  productionUrl: "mongodb://laide:developerlydex26@ds041238.mongolab.com:41238/expense-manager"
+  productionUrl: "mongodb://laide:laide@ds035137.mongolab.com:35137/expense-manager"
 };
