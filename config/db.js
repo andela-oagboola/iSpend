0e1cf731eb8d6f11d21c38f470eb77dfc951b5a3
Andela Dev <olaide.agboola@andela.co>
2015-03-27 09:12:41 +0100

    fixing db connections

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   developmentUrl: "mongodb://localhost/expense-manager",
-  productionUrl: "mongodb://laide:laide@ds035137.mongolab.com:35137/expense-manager"
+  productionUrl: process.env.MONGODB_URL
 };
